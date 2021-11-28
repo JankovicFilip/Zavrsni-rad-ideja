8f549341ccd6dad0a6a123680f32b28339797493
JankovicFilip <93282802+JankovicFilip@users.noreply.github.com>
2021-11-28 14:00:56 +0100

    Update za opis i nayiv grafičkih kartica

--- IToprematest.sql
+++ IToprematest.sql
@@ -11,15 +11,15 @@ use IToprematest;
 
 create table komponenta(
     sifra int not null primary key auto_increment,
-    naziv varchar(50) not null,
-    opis varchar(150) not null,
+    naziv varchar(100) not null,
+    opis varchar(1000) not null,
     cijena decimal(18,2) not null
 );
 
 create table gamingxracunalo(
     sifra int not null primary key auto_increment,
-    naziv varchar(50) not null,
-    opis varchar(200) not null,
+    naziv varchar(100) not null,
+    opis varchar(1000) not null,
     cijena decimal(18,2) not null
 );
 
@@ -70,16 +70,91 @@ insert into kupac (sifra,ime,prezime,email,adresa,brojtelefona) values
 # grafičke kartice su od 1-10
 
 insert into komponenta (sifra,naziv,opis,cijena) values
-(null,'Grafička kartica','Grafička kartica XFX AMD Radeon RX 550, 2 GB GDDR5 (RX-550P2PFG5)',1216.96),
-(null,'Grafička kartica','Grafička kartica ASUS AMD Radeon ROG-STRIX-LC-RX6900XT-T16G-GAMING, 16 GB GDDR6, vodeno hlađenje',11026.43),
-(null,'Grafička kartica','Grafička kartica SAPPHIRE AMD Radeon RX 6900 XT Toxic Extreme Edition Gaming, 16 GB GDDR6, AIO Liquid Cooler 360mm, 11308-08-20G',17179.36),
-(null,'Grafička kartica','Grafička kartica ASUS AMD Radeon R7240-2GD5-L, 2 GB GDDR5',748.45),
-(null,'Grafička kartica','Grafička kartica SAPPHIRE AMD Radeon 550 Pulse, 2 GB GDDR5, 64-bit (11268-21-20G)',1269.94),
-(null,'Grafička kartica','Grafička kartica PCI-E GAINWARD GeForce RTX 3080 Phantom+, LHR, 10GB GDDR6X',13999.00),
-(null,'Grafička kartica','Grafička kartica PCI-E GAINWARD GeForce RTX 3070 Phantom+, LHR, 8GB GDDR6X',9399.00),
-(null,'Grafička kartica','Grafička kartica ASUS nVidia GeForce PH-GTX1650-O4GD6-P, 4 GB GDDR6',2702.45),
-(null,'Grafička kartica','Grafička kartica PCI-E ASUS GeForce RTX 3060TI OC LHR, 8GB GDDR6',7699.00),
-(null,'Grafička kartica','Grafička kartica PCI-E GIGABYTE Radeon RX 6700 XT Gaming OC, 12GB GDDR6',7999.00);
+(null,'Grafička kartica XFX AMD Radeon RX 550, 2 GB GDDR5 (RX-550P2PFG5)','XFX Video Card AMD Radeon RX-550, 2GB 128bit DDR5, 1203/ 7000, PCI-E 3, DP, HDMI, DL-DVI-D, Single Fan, 1 slot',1216.96),
+(null,'Grafička kartica ASUS AMD Radeon ROG-STRIX-LC-RX6900XT-T16G-GAMING, 16 GB GDDR6, vodeno hlađenje','Bus Standard PCI Express 4.0
+OpenGL OpenGL®4.6
+Video Memory 16GB GDDR6
+Engine Clock Gaming mode : Up to 2525 MHz (Boost Clock)/ Up to 2375 MHz (Game Clock)
+Stream Processors 5120
+Memory Speed 16 Gbps
+Memory Interface 256-bit
+Resolution Digital Max Resolution 7680 x 4320',11026.43),
+(null,'Grafička kartica SAPPHIRE AMD Radeon RX 6900 XT Toxic Extreme Edition Gaming, 16 GB GDDR6, AIO Liquid Cooler 360mm','Proizvođač grafičkog procesora (R)	AMD
+Grafički procesor (R)	AMD Radeon RX 6900
+Frekvencija grafičkog procesora VGA (R)	2250 MHz
+Vrsta memorije VGA (R)	GDDR6
+Kapacitet memorije VGA (R)	16 GB
+Frekvencija memorije VGA (R)	16000 MHz
+Sučelje VGA (R)	PCI-Express 4.0
+Propusnost sabirnice (R)	256 bit
+Instalation slots (R)	2.5 slota',17179.36),
+(null,'Grafička kartica ASUS AMD Radeon R7240-2GD5-L, 2 GB GDDR5','Proizvođač grafičkog procesora (R)	AMD
+Grafički procesor (R)	AMD Radeon R7 240
+Frekvencija grafičkog procesora VGA (R)	780 MHz
+Vrsta memorije VGA (R)	GDDR5
+Kapacitet memorije VGA (R)	2 GB
+Frekvencija memorije VGA (R)	1100 MHz
+Sučelje VGA (R)	PCI-Express 3.0
+Propusnost sabirnice (R)	128 bit
+Instalation slots (R)	1 slot
+Tip napajanja VGA (R)	1x 8-pin',748.45),
+(null,'Grafička kartica SAPPHIRE AMD Radeon 550 Pulse, 2 GB GDDR5, 64-bit','Proizvođač grafičkog procesora (R)	AMD
+Grafički procesor (R)	AMD Radeon RX 550
+Frekvencija grafičkog procesora VGA (R)	1206 MHz
+Vrsta memorije VGA (R)	GDDR5
+Kapacitet memorije VGA (R)	2 GB
+Frekvencija memorije VGA (R)	7000 MHz
+Sučelje VGA (R)	PCI-Express 3.0
+Propusnost sabirnice (R)	128 bit',1269.94),
+(null,'Grafička kartica PCI-E GAINWARD GeForce RTX 3080 Phantom+, LHR, 10GB GDDR6X','Sučelje PCI-E
+Radni takt GPU [MHz] 1755
+Vrsta memorije GDDR6X
+Radna memorija (RAM) 10 GB
+Memorijsko sučelje [bit] 320
+D-SUB n/a
+HDMI 1
+DisplayPort 3
+DVI n/a
+Proizvođač čipa nVidia',13999.00),
+(null,'Grafička kartica PCI-E GAINWARD GeForce RTX 3070 Phantom+, LHR, 8GB GDDR6X','Sučelje: PCI-E
+Radni takt GPU [MHz]: 1440
+Vrsta memorije: GDDR6X
+Radna memorija (RAM): 8 GB
+Memorijsko sučelje [bit]: 320
+D-SUB: n/a
+HDMI: 1
+DisplayPort: 3
+DVI: n/a
+Proizvođač čipa: nVidia',9399.00),
+(null,'Grafička kartica ASUS nVidia GeForce PH-GTX1650-O4GD6-P, 4 GB GDDR6','Sučelje PCI-E
+Radni takt GPU [MHz] 1620
+Vrsta memorije GDDR6
+Radna memorija (RAM) 4 GB
+Memorijsko sučelje [bit] 128
+D-SUB n/a
+HDMI 1
+DisplayPort n/a
+USB-C n/a
+DVI 1
+Proizvođač čipa nVidia',2702.45),
+(null,'Grafička kartica PCI-E ASUS GeForce RTX 3060TI OC LHR, 8GB GDDR6','Sučelje	PCI-E
+Grafika model	RTX 3060TI
+Proizvođač čipa	nVidia
+Vrsta memorije	DDR6
+Radni takt [MHz]	1860
+Radna memorija [GB]	8
+Memorijska sabirnica [bit]	192',7699.00),
+(null,'Grafička kartica PCI-E GIGABYTE Radeon RX 6700 XT Gaming OC, 12GB GDDR6','Sučelje PCI-E
+Radni takt GPU [MHz]: TBD
+Vrsta memorije GDDR6
+Radna memorija (RAM) 12 GB
+Memorijsko sučelje [bit] 192
+D-SUB n/a
+HDMI 2
+DisplayPort 2
+DVI n/a
+USB-Cn/a
+Proizvođač čipa AMD',7999.00);
 
 
 
